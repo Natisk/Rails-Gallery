8029b65a8cda8e0246c0a654bff5d713504d52a8
Vitaly <realnatisk@gmail.com>
2013-09-11 18:01:01 +0300

    category has many users through subscribe

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,6 +4,7 @@ class Category < ActiveRecord::Base
 
   has_many :images, dependent: :destroy
   has_many :subscribes
+  has_many :users, through: :subscribes
 
   validates :title, presence: true, length: {minimum: 2, maximum: 255}
 
