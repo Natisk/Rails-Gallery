7f925730648c8a27effc162277b50b008c40abec
Vitaly <realnatisk@gmail.com>
2013-09-12 18:14:02 +0300

    Mail to subscribed users done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,18 +2,10 @@ class UserMailer < ActionMailer::Base
 
   default from: Settings.mailer.user_name
 
-  def self.send_new_images
-    category = Category.where('id = :id', id: params[:category_id]).first
-    @recipients = category.users
-    @recipients.each do |recipient|
-      new_images(recipient).deliver
-    end
-  end
-
   def new_images(recipient)
     @url  = root_url
     @name = recipient.name
-    mail(to: recipient.email, subject: 'New Image has been created')
+    mail(to: recipient.email, subject: " Hi #{@name}! New Image has been created")
   end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,8 @@ class Image < ActiveRecord::Base
 
   mount_uploader :img_name, ImageUploader
 
+  after_create  :user_sender
+
   attr_accessible :title, :img_name, :category_id
 
   has_many :likes
@@ -10,5 +12,12 @@ class Image < ActiveRecord::Base
 
   validates :title, presence: true, length: {minimum: 3, maximum: 255}
   validates :img_name, presence: true
+
+  def user_sender
+    category = Category.includes(:users).find(self.category_id)
+    category.users.each do |recipient|
+      UserMailer.new_images(recipient).deliver
+    end
+  end
 
 end
