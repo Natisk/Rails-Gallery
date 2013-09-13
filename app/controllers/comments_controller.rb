399bab837c9c55fb5a90a8f1263a05fcd29c93bf
Vitaly <realnatisk@gmail.com>
2013-09-13 20:03:00 +0300

    styles for images categories and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def index
-    @comments = Comment.order('created_at DESC')
+    @comments = Comment.order('created_at DESC').includes(:image, :user).page(params[:page]).per(10)
   end
 
   def show
@@ -9,13 +9,11 @@ class CommentsController < ApplicationController
   end
 
   def create
-    #@user = current_user
     if user_signed_in?
       @image = Image.find(params[:image_id])
       @comment = @image.comments.new(params[:comment])
       @comment.user = current_user
       if @comment.save
-        #UserMailer.new_images(@user).deliver
         redirect_to image_path(@image)
       else
         redirect_to :back, notice: 'Body minimum 2 maximum 255 symbols'
--- app/models/image.rb
+++ app/models/image.rb
@@ -6,8 +6,8 @@ class Image < ActiveRecord::Base
 
   attr_accessible :title, :img_name, :category_id
 
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :category, counter_cache: true
 
   validates :title, presence: true, length: {minimum: 3, maximum: 255}
