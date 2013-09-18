aadddd0bf9be7697aaa2fa0ba61ce43b439eeca3
Vitaly <realnatisk@gmail.com>
2013-09-18 14:47:15 +0300

    likes model after create+ before destroy - track like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,7 +6,6 @@ class LikesController < ApplicationController
     @like = current_user.likes.new(image_id: params[:image_id])
     if @like.save
       render nothing: true
-      Event.create(user_id: current_user.id, related_id: @like.id, user_action: 'like add')
     else
       render nothing: true
     end
@@ -16,12 +15,8 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.where('user_id = :user_id AND image_id = :image_id', user_id: current_user.id,
                        image_id: params[:image_id]).first
-    if @like.destroy
-      render nothing: true
-      Event.create(user_id: current_user.id, related_id: @like.id, user_action: 'like del')
-    else
-      render nothing: true
-    end
+    @like.destroy
+    render nothing: true
   end
 
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,19 @@
 class Like < ActiveRecord::Base
 
+  after_create :track_added_like
+  before_destroy :track_deleted_like
+
   attr_accessible :user_id, :image_id
 
   belongs_to :user, counter_cache: true
   belongs_to :image, counter_cache: true
+
+  def track_added_like
+    Event.create(user_id: self.user.id , related_id: self.id, user_action: 'like add')
+  end
+
+  def track_deleted_like
+    Event.create(user_id: self.user.id , related_id: self.id, user_action: 'like del')
+  end
 
 end
