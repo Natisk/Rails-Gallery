739f821cabae27fd7c3d85a556ef9cc45246f050
Vitaly <realnatisk@gmail.com>
2013-09-20 19:22:14 +0300

    parser image url + register_page form

--- app/admin/image_parser.rb
+++ app/admin/image_parser.rb
@@ -1,6 +1,10 @@
 ActiveAdmin.register_page 'Image Parser' do
 
-  page_action :parse_url
+  page_action :parse_url, method: :post do
+    url = params[:parse_link]
+    page = Nokogiri::HTML(open(url))
+    images = page.css('img')['src']
+  end
 
   sidebar :tips do
     ul do
@@ -9,7 +13,12 @@ ActiveAdmin.register_page 'Image Parser' do
   end
 
   content do
-    para 'Hello World'
+    semantic_form_for :image_parser, url: admin_image_parser_url, builder: ActiveAdmin::FormBuilder do |f|
+      f.inputs 'Url to parse' do
+        f.input :parse_link
+      end
+      f.actions
+    end
   end
 
 end
