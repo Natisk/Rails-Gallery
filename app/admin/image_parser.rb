479385a26b9345e4807af2020e7c7acb57fc7549
Vitaly <realnatisk@gmail.com>
2013-09-26 12:46:03 +0300

    site_root out of IF

--- app/admin/image_parser.rb
+++ app/admin/image_parser.rb
@@ -7,6 +7,7 @@ ActiveAdmin.register_page 'Image Parser' do
     @images = Array.new
     @categories = Category.all
     @url = params[:image_parser][:parse_link]
+    site_root = @url.split('/')[0] + '//' + @url.split('/')[2]
     valid_url = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix.match(@url).nil?
     if valid_url
       redirect_to :back, alert: 'Invalid domain name'
@@ -16,7 +17,6 @@ ActiveAdmin.register_page 'Image Parser' do
         if img['src'].include? ('http')
           @images << img['src']
         else
-          site_root = @url.split('/')[0] + '//' + @url.split('/')[2]
           @images << site_root + img['src']
         end
       end
