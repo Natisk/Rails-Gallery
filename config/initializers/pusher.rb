e076ccbe1ca1a15831656c89b0646e7f23f3f3f5
Vitaly <realnatisk@gmail.com>
2013-09-27 14:03:48 +0300

    settings or slanger

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,8 +1,10 @@
 require 'pusher'
 
-Pusher.url = 'http://b15da0d38e1cf270a7b1:a8e7ec425dac974f5bb7@api.pusherapp.com/apps/55139'
+#Pusher.url = 'http://b15da0d38e1cf270a7b1:a8e7ec425dac974f5bb7@api.pusherapp.com/apps/55139'
 Pusher.logger = Rails.logger
 
 Pusher.app_id = Settings.pusher.app_id
 Pusher.key = Settings.pusher.key
 Pusher.secret = Settings.pusher.secret
+Pusher.host   = Settings.pusher.api_host
+Pusher.port   = Settings.pusher.api_port
