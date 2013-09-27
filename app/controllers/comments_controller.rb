f4a5b83249e3d663daf04884258390cf89e10fd5
Vitaly <realnatisk@gmail.com>
2013-09-27 20:08:12 +0300

    comments controller - remove pusher settings

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,3 @@
-require 'pusher'
-
-#Pusher.url = 'http://b15da0d38e1cf270a7b1:a8e7ec425dac974f5bb7@api.pusherapp.com/apps/55139'
-Pusher.logger = Rails.logger
-
-Pusher.app_id = '1'
-Pusher.key = 'c46c644b78f84661ace01b35dffceabc'
-Pusher.secret = '15c85d0ce00c492888423aca73f65d19'
-Pusher.host = '127.0.0.1'
-Pusher.port = '4567'
-
 class CommentsController < ApplicationController
 
   before_filter :authenticate_user!, only: [:create]
