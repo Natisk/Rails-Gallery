e78cacdbfd869d0d1a797effeb2af75d339f9804
Vitaly <realnatisk@gmail.com>
2013-09-30 14:24:42 +0300

    pusher ping pong with alert

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -7,4 +7,4 @@ Pusher.app_id = '1'
 Pusher.key = 'c46c644b78f84661ace01b35dffceabc'
 Pusher.secret = '15c85d0ce00c492888423aca73f65d19'
 Pusher.host = 'localhost'
-Pusher.port = '4567'
+Pusher.port = 4567
