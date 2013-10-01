09e56d68d0bdd79a99f707b89e97cc839077d524
Vitaly <realnatisk@gmail.com>
2013-10-01 19:29:52 +0300

    refactor slider - after click continue slide from current image

--- vendor/assets/javascripts/my_slider_script.js
+++ vendor/assets/javascripts/my_slider_script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
-	
-	var totWidth=0;
+
+    // The number of seconds that the slider will auto-advance in:
+    var changeEvery = 2;
+    var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
+    var totWidth=0;
 	var positions = new Array();
+    var current = 1;
+
+    function autoAdvance()
+    {
+        if(current == -1) return false;
+
+        $('#menu ul li span').eq(current%5).trigger('click',[true]);	// [true] will be passed as the keepScroll parameter of the click function on line 28
+        current++;
+//        if(current > 5) {
+//            current = 5;
+//        }
+    }
+
+    $('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact');
 	
 	$('#slides .slide').each(function(i){
 		
@@ -23,12 +39,9 @@ $(document).ready(function(){
 	});
 	
 	$('#slides').width(totWidth);
-
 	/* Change the cotnainer div's width to the exact width of all the slides combined */
 
-	$('#menu ul li span').click(function(e,keepScroll){
-
-			/* On a thumbnail click */
+	$('#menu ul li span').click(function(e,keepScroll) {
 
 			$('li.menuItem').removeClass('act').addClass('inact');
 			$(this).parent().addClass('act');
@@ -44,34 +57,21 @@ $(document).ready(function(){
 			
 			
 			// Stopping the auto-advance if an icon has been clicked:
-			if(!keepScroll) clearInterval(itvl);
+			if(!keepScroll){
+                clearInterval(itvl);
+                current = parseInt($(this).attr('id'));
+                itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
+            }
+
+
+//            current = parseInt($(this).attr('id'));
+//            current = 1
+//            current ++;
+//            alert(current);
+//            clearInterval(itvl);
+//            itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
 	});
-	
-	$('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact');
-	/* On page load, mark the first thumbnail as active */
-	
-	
-	
-	/*****
-	 *
-	 *	Enabling auto-advance.
-	 *
-	 ****/
-	 
-	var current=1;
-	function autoAdvance()
-	{
-		if(current==-1) return false;
-		
-		$('#menu ul li span').eq(current%$('#menu ul li span').length).trigger('click',[true]);	// [true] will be passed as the keepScroll parameter of the click function on line 28
-		current++;
-	}
-
-	// The number of seconds that the slider will auto-advance in:
-	
-	var changeEvery = 5;
-
-	var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
 
 	/* End of customizations */
+
 });
