4bb7e507636cbb5a36f02e33174d2905b82707f9
Vitaly <realnatisk@gmail.com>
2013-10-03 14:14:07 +0300

    Settings for pusher.rb development

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,8 +3,8 @@ require 'pusher'
 #Pusher.url = 'http://b15da0d38e1cf270a7b1:a8e7ec425dac974f5bb7@api.pusherapp.com/apps/55139'
 Pusher.logger = Rails.logger
 
-Pusher.app_id = '1'
-Pusher.key = 'c46c644b78f84661ace01b35dffceabc'
-Pusher.secret = '15c85d0ce00c492888423aca73f65d19'
-Pusher.host = 'localhost'
-Pusher.port = 4567
+Pusher.app_id = Settings.pusher.app_id
+Pusher.key = Settings.pusher.key
+Pusher.secret = Settings.pusher.secret
+Pusher.host = Settings.pusher.api_host
+Pusher.port = Settings.pusher.port
