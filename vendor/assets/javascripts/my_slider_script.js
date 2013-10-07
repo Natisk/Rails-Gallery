1bd658a9d3e2c5fd8876270789be744fd23f88c6
Vitaly <realnatisk@gmail.com>
2013-10-07 20:18:54 +0300

    js tab-size decreased

--- vendor/assets/javascripts/my_slider_script.js
+++ vendor/assets/javascripts/my_slider_script.js
@@ -1,40 +1,38 @@
 $(document).ready(function() {
 
-    var changeEvery = 5;
-    var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
-    var totWidth=0;
+  var changeEvery = 5;
+  var itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
+  var totWidth=0;
 	var positions = new Array();
-    var current = 1;
+  var current = 1;
 
-    function autoAdvance() {
-        $('#menu li span').eq(current%$('#menu li span').length).trigger('click',[true]);
-        current++;
-    }
+  function autoAdvance() {
+    $('#menu li span').eq(current%$('#menu li span').length).trigger('click',[true]);
+    current++;
+  }
 
-    $('#menu li.menuItem:first').addClass('act').siblings().addClass('inact');
+  $('#menu li.menuItem:first').addClass('act').siblings().addClass('inact');
 	
 	$('#slides .slide').each(function(i) {
-		
 		positions[i]= totWidth;
 		totWidth += $(this).width();
-
 	});
 	
 	$('#slides').width(totWidth);
 
 	$('#menu li span').click(function(e,keepScroll) {
-        $('li.menuItem').removeClass('act').addClass('inact');
-        $(this).parent().addClass('act').removeClass('inact');
+    $('li.menuItem').removeClass('act').addClass('inact');
+    $(this).parent().addClass('act').removeClass('inact');
 
-        var pos = $(this).parent().prevAll('.menuItem').length;
+    var pos = $(this).parent().prevAll('.menuItem').length;
 
-        $('#slides').stop().animate({marginLeft:-positions[pos]+'px'},450);
+    $('#slides').stop().animate({marginLeft:-positions[pos]+'px'},450);
 
-        if(!keepScroll) {
-            clearInterval(itvl);
-            current = parseInt($(this).attr('id'));
-            itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
-        }
+    if(!keepScroll) {
+      clearInterval(itvl);
+      current = parseInt($(this).attr('id'));
+      itvl = setInterval(function(){autoAdvance()},changeEvery*1000);
+    }
 	});
 
 });
