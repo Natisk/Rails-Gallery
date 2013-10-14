f9f2858b0f3fea4bd406706bdd6a0716fb3350a2
Vitaly <realnatisk@gmail.com>
2013-10-14 11:26:00 +0300

    tests changes

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Category do
 
-  context do
+  context 'relations and validations' do
     it {should validate_presence_of(:title)}
+    it {should have_many(:images)}
+    it { should have_many(:images).dependent(:destroy) }
   end
 
 end
