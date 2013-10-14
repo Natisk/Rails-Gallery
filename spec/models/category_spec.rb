83dd7e6c53c8947dfde55576b7b19d2b357c6b51
Vitaly <realnatisk@gmail.com>
2013-10-14 12:15:52 +0300

    category model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe Category do
 
   context 'relations and validations' do
-    it {assert validate_presence_of(:title)}
-    it { should have_many(:images).dependent(:destroy) }
+    it {should validate_presence_of(:title)}
+    it {should ensure_length_of(:title).is_at_least(2).is_at_most(255)}
+    it {should have_many(:images).dependent(:destroy)}
+    it {should have_many(:subscribes)}
+    it {should have_many(:users).through(:subscribes)}
+    it {should allow_mass_assignment_of(:title) }
   end
 
 end
