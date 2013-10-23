cf81ed3588b6306a4d141aa4de88f840b0f0a32c
Vitaly <realnatisk@gmail.com>
2013-10-23 11:29:02 +0300

    comments relations and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Comment do
 
-  it {should belong_to(:user)}
+  context 'relations & validations' do
+    it {should belong_to(:user)}
+    it {should belong_to(:image)}
+    it {should validate_presence_of}
+  end
 
 end
