0d0d1b41f7166ee05340e5010ea88f786131bf37
Vitaly <realnatisk@gmail.com>
2013-10-23 15:40:50 +0300

    comments model test

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :image do
     title 'My image'
     img_name {File.open(File.join(Rails.root, 'spec', 'factories', 'files', 'rails.png'))}
+    association :category, factory: :category
   end
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,14 +9,18 @@ describe Comment do
     it {should ensure_length_of(:body).is_at_least(2).is_at_most(255)}
     it {should validate_presence_of(:user_id)}
     it {should validate_numericality_of(:user_id).only_integer}
-    it {should allow_mass_assignment_of(:body) }
+    it {should allow_mass_assignment_of(:body)}
+    it {should allow_mass_assignment_of(:image_id)}
 
     it 'must track the comment with creating an event' do
-      fu = FactoryGirl.create(:user)
-      fu.name.should eq('Test Usver')
-      #fu.comments.create(body: 'my first comment', image_id: 1)
-      #ev = Event.all
-      #ev.count.should eq(1)
+      fg_user = FactoryGirl.create(:user)
+      fg_image = FactoryGirl.create(:image)
+      fg_user.name.should eq('Test Usver')
+      fg_comment = fg_image.comments.new(body: 'my first comment')
+      fg_comment.user_id = fg_user.id
+      fg_comment.save
+      events = Event.all
+      events.count.should eq(1)
     end
   end
 
