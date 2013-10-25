0c38ddaa3482780a253e2b9909b107d7c75f7378
Vitaly <realnatisk@gmail.com>
2013-10-25 18:16:43 +0300

    comments_controller tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,18 +3,35 @@ require 'spec_helper'
 describe CommentsController do
 
   before(:each) do
-    user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
     @image = FactoryGirl.create(:image)
-    5.times {user.comments.create(image_id: @image, body: 'nice comment')}
+    5.times {@image.comments.create(body: 'nice comment')}
+    request.env['HTTP_REFERER'] = '/my_way'
   end
 
   context 'index test' do
     it 'assigns @comments' do
       get :index
-      assigns(:comments).include?(Comment.last).should be true
       assert_template :index
       assert_template layout: 'layouts/application'
       assert_response :success
+    end
+  end
+
+  context 'create test' do
+    it 'assigns @comment' do
+      sign_in @user
+      post :create, comment: {body: 'sgdgrgergeg'}, image_id: @image
+      comment = Comment.last
+      assigns[:comment].should eq(comment)
+      flash[:notice].should be_nil
+      response.should redirect_to(image_path(@image))
+    end
+    it 'comment not valid' do
+      sign_in @user
+      post :create, comment: {body: 's'}, image_id: @image
+      flash[:notice].should eq('Body minimum 2 maximum 255 symbols')
+      assert_redirected_to('/my_way')
     end
   end
 
